42a9f5e463261f24f1f0f003bc4d2128a231678d
Adam Braden <adambr@microsoft.com>
2018-10-05 09:19:11 -0700

    Inital sample

---
+++ WinForms-WebView-Demo/apisDemo.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace WinForms_WebView_Demo
+{
+    partial class apisDemo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 71);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get SystemId";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.GetSystemId);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(529, 260);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(326, 63);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Show Picture of the Day";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.ShowPictureOfTheDay);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(189, 83);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1123, 71);
+            this.textBox1.TabIndex = 2;
+            // 
+            // apisDemo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1324, 335);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "apisDemo";
+            this.Text = "Windows API Demo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ WinForms-WebView-Demo/apisDemo.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+//
+//Needed for extention methods on windows types, ie: .ToArray()
+using System.Runtime.InteropServices.WindowsRuntime;
+//
+//Notifications & Windows XML APIs
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+
+
+namespace WinForms_WebView_Demo
+{
+    public partial class apisDemo : Form
+    {
+        public apisDemo()
+        {
+            InitializeComponent();
+        }
+
+        private void GetSystemId(object sender, EventArgs e)
+        {
+            Windows.Storage.Streams.IBuffer buffId = Windows.System.Profile.SystemIdentification.GetSystemIdForPublisher().Id;
+
+            textBox1.Text = ByteArrayToHexString(buffId.ToArray());
+        }
+
+        private void ShowPictureOfTheDay(object sender, EventArgs e)
+        {
+            string title = "featured picture of the day";
+            string content = "beautiful scenery";
+            string image = "https://picsum.photos/360/180?image=104";
+            string logo = "https://picsum.photos/64?image=883";
+
+            string xmlString =
+            $@"<toast><visual>
+                <binding template='ToastGeneric'>
+                <text>{title}</text>
+                <text>{content}</text>
+                <image src='{image}'/>
+                <image src='{logo}' placement='appLogoOverride' hint-crop='circle'/>
+                </binding>
+                </visual></toast>";
+
+            XmlDocument toastXml = new XmlDocument();
+            toastXml.LoadXml(xmlString);
+
+            //Create & Show toast
+            ToastNotification toast = new ToastNotification(toastXml);
+            ToastNotificationManager.CreateToastNotifier().Show(toast);
+        }
+
+        /// <summary>
+        /// Helper method to format byte array into a hex string
+        /// </summary>
+        /// <param name="ba"></param>
+        /// <returns>Hex String</returns>
+        public static string ByteArrayToHexString(byte[] ba)
+        {
+            return BitConverter.ToString(ba).Replace("-", "");
+        }
+    }
+}
